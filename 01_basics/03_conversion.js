9f5912dbd10f60166547001a32e7cde71d4da2a4
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-03 07:10:00 +0000

    conversion added

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score=true
+
+console.log(typeof score)
+console.log(typeof (score));
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+/*
+"33"=>33
+"33asdf"=>NaN
+true=>1
+null=>0
+undefined=>NaN
+*/
+let isLoggedIn="Mihir"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1=>true 0=>false
+""=>false
+"mihir"=>true
+*/
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
