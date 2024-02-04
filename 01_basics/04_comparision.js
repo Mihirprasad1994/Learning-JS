35e09e9c5bcfa5f236a6552e478c8de5048055cb
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-04 06:00:27 +0000

    comparision added

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,5 @@
+/*
+jadi number ku number saha kimba string ku string saha compare kara jae the no problem 
+ different data type ku normal comparision kariba is not a good practice
+ === strictly compare kare
+ */
