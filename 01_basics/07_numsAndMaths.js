5e97b80688630be36cee795459da127c87affa7b
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-05 07:16:41 +0000

    random method of Math library added

--- 01_basics/07_numsAndMaths.js
+++ 01_basics/07_numsAndMaths.js
@@ -15,3 +15,6 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.7));
 console.log(Math.ceil(4.2));
 console.log(Math.floor(4.9));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*99+100));
