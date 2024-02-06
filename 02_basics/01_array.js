97022b3136f1114d4f0887f7dd01b3417d21af9d
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-06 11:25:14 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+const myArr=[0,1,2,3,4,5]
+const myHeros=["shaktima","nagraj"]
+
+const myArr2=new Array(1,2,3,4)
+console.log(myArr2[1]);
+
+// Methods of Array 
+// Push pop unshift shift 
+// includes indexOf join=>converts the array into string
+
+/*
+difference between slice and splice
+slice=>doesnot maipulate origial array
+splice=>manipulates original array
+*/
