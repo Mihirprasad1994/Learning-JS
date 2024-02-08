1fa7d5d1a28be85bf3492469b2c89e421a8d2893
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-08 07:31:03 +0000

    rivision on num&math,Date

--- 01_basics/07_numsAndMaths.js
+++ 01_basics/07_numsAndMaths.js
@@ -10,11 +10,30 @@
 // const hundreds=1000000000
 // console.log(hundreds.toLocaleString("en-IN"));
 // **********************************************************
-console.log(Math)
-console.log(Math.abs(-4));
-console.log(Math.round(4.7));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.random());
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*99+100));
+
+// *******************************************************************
+// const number=100
+// const balance=new Number(1000000000)
+// console.log(typeof balance)
+// console.table([number,balance])
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+// console.log(balance.toLocaleString("en-IN"));
+
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(12.456));
+console.log(Math.ceil(12.98765));
+console.log(Math.floor(12.98765));
 console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*99+100));
+console.log(Math.floor(Math.random()*100)+100);
--- 01_basics/08_dates.js
+++ 01_basics/08_dates.js
@@ -15,10 +15,18 @@ console.log(myDate.toLocaleDateString());
 */
 
 // let myCreatedDate= new Date(2024,1,5);
-let myCreatedDate= new Date("02-05-2024");
+// let myCreatedDate= new Date("02-05-2024");
 // console.log(myCreatedDate.toDateString())
-let myTimeStamp=Date.now()
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+//  ***************************************************** 
+
+myCreatedDate=new Date("05-15-2023")
+console.log(myCreatedDate.toString());
+myTimeStamp= Date.now()
 console.log(myTimeStamp);
 console.log(myCreatedDate.getTime());
-
-console.log(Math.floor(Date.now()/1000));
+console.log(Math.floor(myCreatedDate.getTime()/1000));
