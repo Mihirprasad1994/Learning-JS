4e5221bfae8b4d70ca1966a01d0d15d4fb5e0a8c
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-09 06:30:26 +0000

    function started

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+// function=> 10 ba 15 line ra code taku ame wrap kari daba.sei wrapping ra reference ku ei ame further kama kari ba
+// function sayMyName(){} 1keyword 2 name 3 parenthisis 4 scope 
+function sayMyName(){
+    console.log("m");
+    console.log("i");
+    console.log("h");
+    console.log("i");
+    console.log("r");
+}
+// sayMyName() 1 reference 2 execution 
+sayMyName()
+
+function addTwoNumbers(number1,number2){
+    let sum=number1+number2
+    return sum
+    console.log("mihir")
+}
+const result=addTwoNumbers(4,5)
+console.log(result)
