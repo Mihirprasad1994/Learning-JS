17163df7b9c87ff490a1a6e49906a610a834f893
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-09 07:45:58 +0000

    object revised

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,32 +2,54 @@
 
 //object literals
 
-const mySym=Symbol("key1")
-const JsUser={
-    name:"mihir",
-    "full name":"mihir prasad bhuyan",
-    [mySym]:"mykey1",
-    age:18,
-    location:"jaipur",
-    email:"mihir@google.com",
-    isLoggedIn:false,
-    lastLoginDays:["Monday","Saturday"]
-}
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"])
-console.log(JsUser[mySym]);
+// const mySym=Symbol("key1")
+// const JsUser={
+//     name:"mihir",
+//     "full name":"mihir prasad bhuyan",
+//     [mySym]:"mykey1",
+//     age:18,
+//     location:"jaipur",
+//     email:"mihir@google.com",
+//     isLoggedIn:false,
+//     lastLoginDays:["Monday","Saturday"]
+// }
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym]);
 
 // Object.freeze(JsUser)
-JsUser.email="mihir@microsoft.com"
-console.log(JsUser.email);
+// JsUser.email="mihir@microsoft.com"
+// console.log(JsUser.email);
 
 
-JsUser.greeting=function(){
-    console.log("hello JS user")
+// JsUser.greeting=function(){
+//     console.log("hello JS user")
+// }
+// JsUser.greetingTwo=function(){
+//     console.log(`i am ${this["full name"]}`);
+// }
+// console.log(JsUser.greeting)
+// console.log(JsUser.greetingTwo())
+// ****************************************************
+
+const userName={
+    name:"mihir",
+    "full name":"mihir prasad bhuyan",
+    address:{
+        present:{
+            villege:{
+                post:{
+                    office:biribati
+                }
+            }
+        }
+    }
 }
-JsUser.greetingTwo=function(){
-    console.log(`i am ${this["full name"]}`);
-}
-console.log(JsUser.greeting)
-console.log(JsUser.greetingTwo())
+console.log(userName.name);
+console.log(userName["full name"]);
+userName.email="asdsdfg@gmail.com"
+Object.freeze(userName)
+userName.address="xcvb"
+console.log(userName);
+
