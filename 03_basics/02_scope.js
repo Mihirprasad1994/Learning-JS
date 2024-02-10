3093bae24cc061e70b184431686cb25281ab1769
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-10 08:12:34 +0000

    scope and hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,3 +8,21 @@ if(true){
 console.log(a);
 // console.log(b);
 console.log(c);
+function one(){
+    let friend1="liku"
+    function two(){
+        let friend2="diza"
+        console.log(friend1);
+    }
+    // console.log(friend2);
+    two()
+}
+one()
+hello()
+function hello(){
+    console.log("hello")
+}
+hi()
+const hi=function(){
+    console.log("hi")
+}
