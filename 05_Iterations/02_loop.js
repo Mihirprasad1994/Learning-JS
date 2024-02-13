020e1d7c328ff2dcb7d0b6eff72a1b226a2e3e6c
Mihirprasad1994 <148360372+Mihirprasad1994@users.noreply.github.com>
2024-02-13 06:41:15 +0000

    while and do while loop finished

---
+++ 05_Iterations/02_loop.js
@@ -0,0 +1,14 @@
+//while
+
+let index=0;
+while(index<=20){
+    console.log(`the value is ${index}`);
+    index+=5
+}
+
+let myArray=["mihir","sameer","hapi","bapi"]
+let element=0;
+while (element<myArray.length) {
+    console.log(`value is ${myArray[element]}`);
+    element+=1
+}
